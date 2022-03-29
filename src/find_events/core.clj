9343d1bfd36eeeec289bf6e913d639c9164af758
L. E. Vandergriff <ed75vande@yahoo.com>
2022-03-29 18:09:56 -0500

    extend tests; add year as param.

--- src/find_events/core.clj
+++ src/find_events/core.clj
@@ -16,27 +16,6 @@
   (format "%2d-%02d %2d:%02d  " (:month v) (:day v) (:hour v) (:minute v) ))
 (defn fhm  "" [v] (format "%2d:%02d " (:hour v) (:minute v)) )
 
-(defn monthly ""
-  ([y m]
-     (let [dim ([0  31 28 31  30 31 30  31 31 30  31 30 31] m) ]
-       (monthly y m 1 dim))
-     )
-  
-  ([y m dSt noD]    ;; arity overloading
-     (loop [d dSt
-            rs (ef/getRiseAndSetTimes ss/SUN y m d  obs)
-            tt (ef/getTransitTimes    ss/SUN y m d obs)   ]
-       (println   (fymd tt)  (fhm (rs 0))  (fhm (rs 1))  (fhm tt))
-       (if (= d noD)
-         0
-         (recur (inc d)
-                (ef/getRiseAndSetTimes ss/SUN y m (inc d)  obs)
-                (ef/getTransitTimes ss/SUN y m (inc d) obs)     ) )
-       ) )
-  )
-
-
-(defn yearly "" [y fnMn] (doseq [m (range 12)] (fnMn y (inc m)))  )
 
 
 (defn printES "" [y]
@@ -50,11 +29,11 @@
 ;;2018-06-20    5:26d   azi 125.206   13:15d   alt  68.469 (decl  23.435)   azi 234.741   21:03d
 
 
-(defn WGdetail "" [y m]
-  (let [rs  (ef/getRiseAndSetTimes ss/SUN y m 20  obs)
-        tt  (ef/getTransitTimes ss/SUN y m 20 obs)  ]
+(defn day-detail "" [y m d]
+  (let [rs  (ef/getRiseAndSetTimes ss/SUN y m d  obs)
+        tt  (ef/getTransitTimes ss/SUN y m d obs)  ]
     (println (fmdhm (rs 0)) (fhm tt) (fhm (rs 1))
-             (format "  %9.5f  %9.5f (%9.5f)"
+             (format "  %10.5f  %9.5f (%9.5f)"
                      (ss/getLongitudeDeg (ss/getHorizontalPosition
                                          ss/SUN (:val (rs 0)) obs))
                      (ss/getAltDeg (ss/getHorizontalPosition
@@ -65,16 +44,76 @@
 ;;(ss/getLongitudeDeg (ss/getHorizontalPosition ss/SUN (:val (rs 1)) obs))
                      ))) )
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
+(defn WGdetail " 20th of month" [y m] (day-detail y m 20 ) )
 
- 
- (monthly 2018 6)
- (WGdetail 2018 6)
-;  (yearly 2018 WGdetail)
+(defn isLeapYear  " 1 ==> leap year"  [year]
+  (cond
+   (= 0 (mod year 400)) 1
+   (= 0 (mod year 100)) 0
+   (= 0 (mod year   4)) 1
+   :else                0)
+  )
 
-;  (printES 2018)
+(defn centuryAnchorDay "" [c]  (+ 2 (* 5 (mod (mod c 4) 7) ))  )
+
+(defn dDayForYear  "day of week of dDay"  [T anchorDay]
+  (let [T0 (if (odd?  T) (+ T  11) T)
+        T1 (/ T0 2)
+        T2 (if (odd? T1) (+ T1 11) T1 )
+        T3 (- 7 (mod T2 7))  ]
+    (mod (+ T3 anchorDay) 7) )
+  )
+
+
+(defn firstWed "DoM for 1st Wed" [y m]
+  (let [ymDdaysDoW ([[0  3  7  7   4  2  6   4  1  5   3  7  5]
+                     [0  4  1  7   4  2  6   4  1  5   3  7  5]] (isLeapYear y))
+        anchor     (centuryAnchorDay (int (/ y 100.0)))
+        DoWdDayYr  (dDayForYear (mod y 100) anchor)
+        delta      [3 2 1 0 -1 -2 -3]
+        propWDoM   (+ (ymDdaysDoW m)  (delta DoWdDayYr))     ]
+    (cond (>  propWDoM 7)  (- propWDoM 7)
+          (<= propWDoM 0)  (+ propWDoM 7)
+          :else             propWDoM      )    )  )
+
+
+(defn monthlyByDayStep "" [fmtFn y m ds step]
+  (let [dim ([[0 31 28 31  30 31 30  31 31 30  31 30 31]
+              [0 31 29 31  30 31 30  31 31 30  31 30 31]] (isLeapYear y))]
+    (loop [d ds]  (fmtFn y m d)
+          (if (> d (- (dim m) step))  0  (recur (+ d step)))  )))
+
+
+(defn monthlyByDay "" [fmtFn y m]  (monthlyByDayStep fmtFn y m 1 1))
+
+
+(defn monthlyByWkWed "" [fmtFn y m]
+  (let [d (firstWed y m)] (monthlyByDayStep fmtFn y m d 7)) )
+
+
+(defn yearlyByDay "" [fmtFn y]
+  (doseq [m (range 1 13)] (monthlyByDay fmtFn y m )))
+
+
+(defn yearlyByWeek "" [fmtFn y]
+  (doseq [m (range 1 13)] (monthlyByWkWed fmtFn y m )))
+
+
+(defn yearlyByMonth "" [ y]  (doseq [m (range 1 13)] (WGdetail  y m )))
+
+(defn aDay "" [y m d] (day-detail y m d) )
+
+
+(defn -main ""  [& args]
+  (let [y  ((vec args) 0)]
+    (printES y)
+    (println)
+    (yearlyByMonth y)
+    (println)
+    (yearlyByWeek day-detail y)
+    (println)
+    (monthlyByDay day-detail y 9)
+    )
   
 ;;2018    3-20 11:15    6-21  5:07    9-22 20:54   12-21 16:22  
 
