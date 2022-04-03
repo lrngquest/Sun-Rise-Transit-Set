a57531abb285af4c046ddaddc32c4833b48ea2ae
L. E. Vandergriff <ed75vande@yahoo.com>
2022-04-02 19:08:06 -0500

    Reorg CLI to short forms for most used cases; Add optional 'observer' config.

--- src/find_events/core.clj
+++ src/find_events/core.clj
@@ -1,14 +1,15 @@
 (ns find-events.core
-  (:require [find-events.ssetc :as ss])
-  (:require [find-events.efx   :as ef])  (:require [find-events.dates :as da])
+  (:import (java.time Instant ZoneOffset OffsetDateTime) )
+  (:require [find-events.ssetc :as ss]
+            [find-events.efx   :as ef]  [find-events.dates :as da]
+            [clojure.edn :as edn]       [clojure.java.io :as io]  )    
   (:gen-class))
+(set! *warn-on-reflection* true)
 
 
-(def obs {:longitude -1.6275195274847123  :latitude 0.7848169687565342
-          :TZoffs    -360    :TZ "US-CENTRAL"  :loc "MSP"} ) ;; TZoffs minutes 
+(def dflt-obs {:longitude -1.6275195274847123  :latitude 0.7848169687565342
+          :TZoffs -360    :TZ "US-CENTRAL"  :loc "MSP."} ) ;; TZoffs minutes
 
-
-(defn fymd "" [v] (format "%4d-%02d-%02d " (:year v)  (:month v) (:day v)) )
 (defn fmdhm "" [v]
   (format "%2d-%02d %2d:%02d  " (:month v) (:day v) (:hour v) (:minute v) ))
 (defn fhm  "" [v] (format "%2d:%02d " (:hour v) (:minute v)) )
@@ -18,7 +19,7 @@
 ;;2018	Mar 20 11:15d	Jun 21 05:07d	Sep 22 20:54d	Dec 21 16:22
 
 
-(defn printES "" [y]
+(defn printES "" [y  obs]
   (let [es  (ef/getEquinoxesAndSolsticesByYear y obs) ]
     (println (format "%4d  " (:year (es 0)) ) 
              (fmdhm (es 0))  (fmdhm (es 1))  (fmdhm (es 2))  (fmdhm (es 3)))  )
@@ -28,12 +29,18 @@
 ;;java -cp test3.jar:t0.jar org.shetline.skyviewcafe.CmdLnRST 2018 6 20
 ;;2018-06-20    5:26d   azi 125.206   13:15d   alt  68.469 (decl  23.435)   azi 234.741   21:03d
 
+(defn dl-hm "subtract times, borrowing as needed" [ev1 ev0]
+  (let [h1  (:hour ev1)   m1  (:minute ev1)
+        h0  (:hour ev0)   m0  (:minute ev0) ]
+    (if (>= m1 m0)  [(- h1 h0) (- m1 m0)]    [(- h1 1 h0) (- (+ m1 60) m0)]) )  )
 
-(defn day-detail "" [y m d]
+
+(defn day-detail "with length of day" [y m d  obs]
   (let [rs  (ef/getRiseAndSetTimes ss/SUN y m d  obs)
-        tt  (ef/getTransitTimes ss/SUN y m d obs)  ]
+        tt  (ef/getTransitTimes ss/SUN y m d obs)
+        [dlh dlm]  (dl-hm (rs 1) (rs 0))  ]  ;; daylight hrs mins
     (println (fmdhm (rs 0)) (fhm tt) (fhm (rs 1))
-             (format "  %10.5f  %9.5f (%9.5f)"
+             (format " %10.5f  %9.5f (%9.5f)  %2dh %2dm"
                      (ss/getLongitudeDeg (ss/getHorizontalPosition
                                          ss/SUN (:val (rs 0)) obs))
                      (ss/getAltDeg (ss/getHorizontalPosition
@@ -42,7 +49,7 @@
                                          ss/SUN (:val tt) obs ss/AbrrNut))
                      ;;could add set azi (...%9.5f)
 ;;(ss/getLongitudeDeg (ss/getHorizontalPosition ss/SUN (:val (rs 1)) obs))
-                     ))) )
+                     dlh dlm))) )
 
 
 (defn isLeapYear  " 1 ==> leap year"  [year]
@@ -53,75 +60,61 @@
    :else                0)
   )
 
-(defn firstWed "DoM for 1st Wed" [y m]
-  (let [ymDdaysDoW ([[0  3  7  7   4  2  6   4  1  5   3  7  5]
-                     [0  4  1  7   4  2  6   4  1  5   3  7  5]] (isLeapYear y))
-        anchor     (da/centuryAnchorDay (int (/ y 100.0)))
-        DoWdDayYr  (da/dDayForYear (mod y 100) anchor)
-        delta      [3 2 1 0 -1 -2 -3]
-        propWDoM   (+ (ymDdaysDoW m)  (delta DoWdDayYr))     ]
-    (cond (>  propWDoM 7)  (- propWDoM 7)
-          (<= propWDoM 0)  (+ propWDoM 7)
-          :else             propWDoM      )    )  )
 
-
-(defn monthlyByDayStep "" [fmtFn y m ds step]
-  (let [dim ([[0 31 28 31  30 31 30  31 31 30  31 30 31]
-              [0 31 29 31  30 31 30  31 31 30  31 30 31]] (isLeapYear y))]
-    (reduce (fn [d v]
-              (fmtFn y m d)
-              (if (> d (- (dim m) step))  (reduced 0) (+ d step) )
-              )
-            ds (range 1 32))   ))
-
-(defn monthlyByDay "" [fmtFn y m]  (monthlyByDayStep fmtFn y m 1 1))
-
-
-(defn monthlyByWkWed "" [fmtFn y m]
-  (let [d (firstWed y m)] (monthlyByDayStep fmtFn y m d 7)) )
-
-
-(defn yearlyByDay "" [fmtFn y]
-  (doseq [m (range 1 13)] (monthlyByDay fmtFn y m )))
-
-
-(defn yearlyByWeek "" [fmtFn y]
-  (doseq [m (range 1 13)] (monthlyByWkWed fmtFn y m )))
-
-
-(defn yearlyByMonth "" [ y]  (doseq [m (range 1 13)] (day-detail  y m 20 )))
-
-
-(defn daysByCount [y m d no]
+(defn daysByStepCount [y m d stsize no  obs]
   (let [dim ([[0 31 28 31  30 31 30  31 31 30  31 30 31]
               [0 31 29 31  30 31 30  31 31 30  31 30 31]] (isLeapYear y))]
     (reduce (fn [[y m d] days]
-            (day-detail y m d)
-            (if (> (inc d) (dim m))
-              (if (= 12 m)  [(inc y) 1 1]   [y (inc m) 1]);; y,m rollover resp
-              [y m (inc d)] ) )  ;;else advance d only
-          [y m d]  (range no)  )   )  )
+              (day-detail y m d  obs)
+  ;; incrementing parts of date with carry-out as needed
+              (let [std          (+ d stsize)
+                    m-end        (dim m)
+                    [lmda coda]  (if (> std m-end)  [(- std m-end) 1]  [std 0])
+                    smn          (+ m coda)
+                    [lmmn comn]  (if (> smn 12)     [1 1]              [smn 0])
+                    styr         (+ y comn)                               ]
+                [styr lmmn lmda] )  )
+          [y m d]  (range no) )   )  )
 
 
-(defn -main ""  [& args]
-  (let [va (vec (map read-string args)) ] ;; y   y m   y m d
-    (case (count va)
-      1  (do (println (va 0))  (yearlyByDay day-detail (va 0)) )
+(defn dDay "simplified " [year]
+  (->> (da/centuryAnchorDay (int (/ year 100.0)) )
+       (da/dDayForYear (mod year 100) ) )  )
+
+
+(defn clix "enhanced cli -- short forms for frequent used cases" [va xobs]
+  (case (count va)   ;; (empty)   y...
+      0  (let [t   (.atOffset (Instant/now ) ZoneOffset/UTC)
+               ny  (.getYear t)   nm (.getMonthValue t)   nd (.getDayOfMonth t)
+               [y m d ]  (->>(- (da/getJD ny nm nd 12 0 0) 7) (da/getDate))  ]
+           (daysByStepCount y m d 1 15  xobs)) ;; 2wk window around today's date
       
-      2  (cond
-          (> (va 1)0)
-          (do (println (va 0))  (monthlyByDay day-detail (va 0) (va 1)) )
-          
-          (= 0 (va 1))
-          (do (printES (va 0) ))  )
+      2  (when (= 0 (va 1)) ;; ala WG sun-decln. diag.
+           (doseq [m (range 1 13)] (day-detail  (va 0) m 20  xobs))
+           (println ) (printES (va 0) xobs)  )
       
-      3  (cond
-          (> (va 1)0)
-          (do (println (va 0))  (day-detail (va 0) (va 1) (va 2)) )
-          
-          (and (= 0 (va 1)) (= 0 (va 2)))
-          (do (println (va 0))  (yearlyByWeek day-detail (va 0)))
-          (and (= 0 (va 1)) (= 1 (va 2)))
-          (do (println (va 0))  (yearlyByMonth (va 0))) ) ;;WG
+      
+      3  (do ;; yr day {52,53}  one-day-per-week  "almanac" case
+           (daysByStepCount (va 0) 1 (va 1) 7 (va 2)  xobs)  (println)
+           (printES (va 0) xobs)
+           (printf "\nleap-year: %d   Dday: %s\n"  (isLeapYear (va 0))
+                   (["Sun""Mon""Tue" "Wed""Thu""Fri""Sat"](dDay (va 0)))) )
+      
+      4  (daysByStepCount (va 0) (va 1) (va 2) 1  (va 3)  xobs)
 
-      4  (do (daysByCount (va 0) (va 1) (va 2)  (va 3)) )   )    )  )
+      
+      (do (print " no args                 ==>  2 week window around today's date\n"
+                  "year 0                  ==>  WG sun-declination diag. case\n"
+                  "year start-day {52,53}  ==>  almanac case\n"
+                  "year month day number-of-days ==> general case\n") )   )
+  )
+
+
+(defn -main "reads 'config.edn' if present as  observer"  [& args]
+  (let [va         (mapv read-string args)
+        cfg-file   (io/file "config.edn")
+        z-obs      (if (.exists cfg-file)
+                     (edn/read-string (slurp cfg-file))
+                     dflt-obs  ) ]    
+    (clix va z-obs)
+    (printf ":loc %s\n" (:loc z-obs)) )  )
